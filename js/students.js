6bfff82f055ad2abf3b83b7142cb027d2dcc3514
rajveersinghjpg <prepwithraj@gmail.com>
2026-02-07 13:48:50 +0530

    Update students.js

--- js/students.js
+++ js/students.js
@@ -1,5 +1,43 @@
 const students = {
 
+"36852": {
+    name: "ARCHIT KUMAR",
+    father: "BRHAM PAL",
+    mother: "REKHA RANI",
+    session: "2025-26",
+    course: "DIPLOMA IN ENGG",
+    stream: "",
+    category: "GENERAL",
+    gender: "MALE",
+    year: "2",
+
+    address: "VILL RUMASA, TEKURU",
+    district: "FIROZABAD",
+    state: "UTTAR PRADESH",
+    country: "INDIA",
+
+    photo: "images/archit.jpg"
+},
+
+"37652": {
+    name: "ABHISHEK PAIL WAR",
+    father: "MAHAVEER SINGH",
+    mother: "-",
+    session: "2024-25",
+    course: "MASTER OF BUSINESS ADMINISTRATION",
+    stream: "MARKETING & HR",
+    category: "OBC",
+    gender: "MALE",
+    year: "1",
+
+    address: "DIGRAUTA, AGRA",
+    district: "AGRA",
+    state: "UTTAR PRADESH",
+    country: "INDIA",
+
+    photo: "images/abhishek.jpg"
+},
+
 "SRU220740087": {
     name: "RUPESH KUMAR SINGH",
     father: "SANTOSH KUMAR SINGH",
@@ -9,46 +47,14 @@ const students = {
     stream: "PCM (NEP)",
     category: "GENERAL",
     gender: "MALE",
-    year: "1",
+    year: "3",
+
     address: "VILL - TIWARPUR",
     district: "VARANASI",
     state: "UTTAR PRADESH",
     country: "INDIA",
+
     photo: "images/rupesh.jpg"
-},
-
-"36852": {
-    name: "ARCHIT KUMAR",
-    father: "BRHAM PAL",
-    mother: "-",
-    session: "2025-26",
-    course: "DIPLOMA IN ENGG (AUTOMOBILE)",
-    stream: "",
-    category: "GENERAL",
-    gender: "MALE",
-    year: "2",
-    address: "",
-    district: "",
-    state: "",
-    country: "INDIA",
-    photo: "images/archit.jpg"
-},
-
-"37652": {
-    name: "ABHISHEK PAIL WAR",
-    father: "MAHAVEER SINGH",
-    mother: "-",
-    session: "2025-26",
-    course: "MASTER OF BUSINESS ADMINISTRATION",
-    stream: "MARKETING & HR",
-    category: "GENERAL",
-    gender: "MALE",
-    year: "1",
-    address: "",
-    district: "",
-    state: "",
-    country: "INDIA",
-    photo: "images/abhishek.jpg"
 }
 
 };
